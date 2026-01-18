a07f7b3f0cc656a6c2c526b4c93d8be5a1a7a49b
evandrolopes85 <evandrocwd@gmail.com>
2026-01-18 01:23:38 -0300

    feature: criando struct e mensagem de inicio

---
+++ war.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+#define QTD_TERRITORIOS 5
+#define MAX_STRING 30
+
+struct Territorio {
+	char nome[30];
+	char cor[10];
+	int tropas;
+}
+
+int main(){
+	printf("----------                 NOVATO                    -------\n\n")
+	printf("--- Vamos cadastrar os territorios inciais do nosso mundo.---\n\n");
+	return 0;
+}
